1516fd05a9a90e0daa6b9b768f5f8a1ad2479039
kantetfrancis05-spec <kantetfrancis05@gmail.com>
2026-06-30 12:54:48 +0300

    Create lib/app/constants.dart

---
+++ lib/lib/app/constants.dart
@@ -0,0 +1,9 @@
+class AppConstants {
+  static const String appName = 'BudgetWatch Kenya';
+
+  static const String tagline = 'Follow Every Shilling.';
+
+  static const String motto = 'Sovereignty Belongs to the People.';
+
+  static const String version = '1.0.0';
+}
